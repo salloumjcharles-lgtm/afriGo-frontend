f7790e49c994df640c08a2105c35883e86198a7f
salloumjcharles-lgtm <salloum.jcharles@gmail.com>
2026-03-04 17:49:28 +0100

    Create DriverLayout component for book screen

---
+++ app/(client)/book.tsx
@@ -0,0 +1,13 @@
+1|import { Stack } from 'expo-router';
+2|
+3|export default function DriverLayout() {
+4|  return (
+5|    <Stack
+6|      screenOptions={{
+7|        headerShown: false,
+8|        contentStyle: { backgroundColor: '#0A0A0A' },
+9|      }}
+10|    />
+11|  );
+12|}
+13|
